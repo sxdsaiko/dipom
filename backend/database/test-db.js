9ce9acc1a92d8361ca137cd4f29992903a6094f2
ruru <zakilopggg1@gmail.com>
2026-05-08 02:06:22 +0300

    Update test-db.js

--- backend/database/test-db.js
+++ backend/database/test-db.js
@@ -1,1 +1,17 @@
+const mysql = require('mysql2/promise');
 
+(async () => {
+  try {
+    const conn = await mysql.createConnection({
+      host: process.env.MYSQLHOST,
+      user: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
+    });
+
+    console.log("✅ MySQL WORKS");
+    await conn.end();
+  } catch (e) {
+    console.error("❌ REAL ERROR:", e);
+  }
+})();
